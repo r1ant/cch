ed1049add76ec2f270b81a4a478aabc8064e3c81
r1ant <86554388+r1ant@users.noreply.github.com>
2021-11-13 16:12:53 +0500

    Update dow.ps1

--- dow.ps1
+++ dow.ps1
@@ -1,2 +1,3 @@
 (New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/r1ant/cch/main/888.ps1", "$env:C:\Users\Administrator\Desktop\xch\888.ps1")
+cd C:\Users\Administrator\Desktop\xch\
 ./888.ps1
