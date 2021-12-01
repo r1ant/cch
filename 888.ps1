7fdbcc4647d4da6ccef4dc7256ff36faa437109d
r1ant <86554388+r1ant@users.noreply.github.com>
2021-12-01 14:52:48 +0500

    Update 888.ps1

--- 888.ps1
+++ 888.ps1
@@ -7,7 +7,7 @@ do
 TIMEOUT /T 8
 .\chia_plot.exe -t X:\1\ -2 R:\ -d X:\chia\ -n 1 -r 12 -u 256  -c xch13qlsja73l2srrg47hxk47q9fsg8mdhd6d0yjlqmg3xy63rvya6aq7cjupt -f 968ea235529fc65c7592fe1a293ccf794baf9b57dd2547f11c47ba0f43a239ad855569f9dabe66056b892e915c6055dc
 TIMEOUT /T 8
-./azcopy copy "X:\chia\*" "https://ilyos111.blob.core.windows.net/ilyos111?sv=2020-08-04&st=2021-11-23T20%3A55%3A33Z&se=2022-03-24T20%3A55%3A00Z&sr=c&sp=racwdxlt&sig=Vk4dolrZwkD55yN%2FgD6qXXWA4bNViUk744P4I6FTtAc%3D" --recursive=true --overwrite=false
+./azcopy copy "X:\chia\*" "https://ilyos222.blob.core.windows.net/ilyos222?sv=2020-08-04&st=2021-12-01T09%3A36%3A51Z&se=2022-05-18T09%3A36%3A00Z&sr=c&sp=racwdxlt&sig=aDyFtUBoaM6%2BIiDaa9VJdkvnXkrAtXdmBJv2A%2FIxekk%3D" --recursive=true --overwrite=false
 TIMEOUT /T 8
 Get-ChildItem -Path X:\chia -File | Remove-Item -Verbose
 clear
